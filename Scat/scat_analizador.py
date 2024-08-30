d0e45405143ebb2eec1d1ddd9eb832e805b88440
mmanzano <71762082+mmanzano2010@users.noreply.github.com>
2024-08-30 11:54:10 +0200

    Uso de constantes en el calculo de intervalos

--- Scat/scat_analizador.py
+++ Scat/scat_analizador.py
@@ -14,7 +14,7 @@ import argparse
 
 
 MIN_LEVEL = -130
-INTERVALO_MAX = 5
+INTERVALO_MAX = 10
 INTERVALO_MIN = 0.25
 
 DIFF_RSRP_MIN = 5
@@ -24,12 +24,12 @@ DIFF_RSRP_EXTR = 20
 APP_LOCALIZACION = 'com.mendhak.gpslogger'
 LOCALIZACION_RUTA_ARCHIVO = '/sdcard/GpsLogger'
 
-def train_model(params, data):
+def train_model(parametros, datos):
     """Entrenamiento del modelo de Aprendizaje Automatico,
     Como argumentos lleva la lista de celdas y los parametros de aprendizaje,
     devuelve un modelo entrenado"""
-    celdas = data
-    x_train = pd.DataFrame(celdas)
+    celdas_entrenamiento = datos
+    x_train = pd.DataFrame(celdas_entrenamiento)
     x_train = x_train.drop('earfcn',axis="columns")
     x_train = x_train.drop('time',axis="columns")
     x_train = x_train.drop('rsrq',axis="columns")
@@ -41,14 +41,14 @@ def train_model(params, data):
     y_train = y_train.to_numpy()
     train_data = lgb.Dataset(x_train, label=y_train)
 
-    if len(celdas) > 15:
-        cv_results = lgb.cv(params, train_data)
+    if len(celdas_entrenamiento) > 15:
+        cv_results = lgb.cv(parametros, train_data)
         best_num_boost_round = len(cv_results)
-        final_model = lgb.train(params, train_data, num_boost_round=best_num_boost_round)
+        final_model = lgb.train(parametros, train_data, num_boost_round=best_num_boost_round)
         return final_model
 
     best_num_boost_round = 1
-    final_model = lgb.train(params, train_data, num_boost_round=best_num_boost_round)
+    final_model = lgb.train(parametros, train_data, num_boost_round=best_num_boost_round)
     return final_model
 
 def coger_datos_geo(localizacion_ruta_archivo):
@@ -62,27 +62,32 @@ def coger_datos_geo(localizacion_ruta_archivo):
     long = last_point.longitude
     altitud = last_point.elevation
     return lat,long,altitud
-def procesar_linea(linea_decod,coordenadas):
+def procesar_linea(linea_a_decodificar, datos_geoloc):
     """Funcion para procesar una linea de analisis de cobertura LTE"""
-    linea_decod = linea_decod.replace(',', ' ,').replace(':', ' :')
-    match = re.search('EARFCN' + r'\s+(\S*)', linea_decod)
+    linea_a_decodificar = linea_a_decodificar.replace(',', ' ,').replace(':', ' :')
+    earfcn = 0
+    pci = 0
+    plmn = 0
+    rsrp = 0.0
+    rsrq = 0.0
+    match = re.search('EARFCN' + r'\s+(\S*)', linea_a_decodificar)
     if match:
         earfcn = int(match.group(1))
-    match = re.search('PCI' + r'\s+(\S*)', linea_decod)
+    match = re.search('PCI' + r'\s+(\S*)', linea_a_decodificar)
     if match:
         pci = int(match.group(1))
-    match = re.search('PLMN' + r'\s+(\S*)', linea_decod)
+    match = re.search('PLMN' + r'\s+(\S*)', linea_a_decodificar)
     if match:
         plmn = int(match.group(1))
-    match = re.search('RSRP :' + r'\s+(\S*)', linea_decod)
+    match = re.search('RSRP :' + r'\s+(\S*)', linea_a_decodificar)
     if match:
         rsrp = float(match.group(1))
-    match = re.search('RSRQ :' + r'\s+(\S*)', linea_decod)
+    match = re.search('RSRQ :' + r'\s+(\S*)', linea_a_decodificar)
     if match:
         rsrq = float(match.group(1))
 
     # Datos de geolocalizacion
-    latitud,longitud,altitud = coordenadas
+    latitud,longitud,altitud = datos_geoloc
     # Procesado de datos de la celda
     serving_cell = {'earfcn': earfcn, 'pci': pci,
                     'plmn': plmn, 'rsrp': rsrp,
@@ -109,27 +114,27 @@ if __name__ == '__main__':
     modelo_procesador = args.modelo
     interfaz = args.interfaz
     bus_usb = funciones.get_interfaz_dispositivo(modelo)
-    bus = str(bus_usb[0])+":"+str(bus_usb[1])
+    BUS = str(bus_usb[0]) + ":" + str(bus_usb[1])
     #Para archivo de guardado
-    date = str(datetime.datetime.now().strftime("%d%m%Y%H%M%S"))
-    file = f'celdas{date}.json'
+    DATE = str(datetime.datetime.now().strftime("%d%m%Y%H%M%S"))
+    file = f'celdas{DATE}.json'
     path_to_file = f'ficheros_mediciones/{file}'
     #Comando de ejecucion de Scat
     comando = ['scat', '-t', funciones.COMANDO_SEGUN_MODELO[modelo_procesador],
-               '-u', '-a', bus, '-i', str(interfaz)]
+               '-u', '-a', BUS, '-i', str(interfaz)]
     proceso = subprocess.Popen(comando,bufsize=1,stdout = subprocess.PIPE, pipesize=1,text = True)
     #Direccion de la app de geolocalizacion y archivo
-    fecha = str(datetime.date.today()).replace('-', '')
-    archivo_localizacion = fecha + '.gpx'
-    LOCALIZACION_RUTA_ARCHIVO = LOCALIZACION_RUTA_ARCHIVO + '/' + archivo_localizacion
+    FECHA = str(datetime.date.today()).replace('-', '')
+    ARCHIVO_LOCALIZACION = FECHA + '.gpx'
+    LOCALIZACION_RUTA_ARCHIVO = LOCALIZACION_RUTA_ARCHIVO + '/' + ARCHIVO_LOCALIZACION
     print(f'Archivo de geolocalizacion:{LOCALIZACION_RUTA_ARCHIVO}')
     funciones.acceder_paquete(APP_LOCALIZACION)
 
     #Variables generales del programa
-    interval = 1
+    INTERVAL = 1
     celdas = []
     celda_ref = {}
-    ciclos = 50
+    CICLOS = 50
     #Parametros de aprendizaje del modelo de AA
     params = {'num_leaves': 31, 'objective': 'regression','n_jobs':4,'learning_rate':0.5}
     params['metric'] = 'rmse'
@@ -137,23 +142,28 @@ if __name__ == '__main__':
     #Programa principal
     try:
         print('Escaneando...')
-        interval_aux = interval
+        INTERVAL_AUX = INTERVAL
         while True:
-            interval = interval_aux
+            INTERVAL = INTERVAL_AUX
 
-            if len(celdas) > ciclos or interval >=9:
+            if len(celdas) > CICLOS or INTERVAL >=9:
                 proceso.terminate()
                 time.sleep(1)
-                proceso = subprocess.Popen(comando, bufsize=1, stdout=subprocess.PIPE, pipesize=1, text=True)
+                proceso = subprocess.Popen(comando,
+                                           bufsize=1,
+                                           stdout=subprocess.PIPE,
+                                           pipesize=1,
+                                           text=True
+                )
                 print("REINICIO SCAT")
-                print(ciclos)
+                print(CICLOS)
                 print(len(celdas))
 
-                ciclos += 50
-                interval = 1
-                interval_aux = interval
+                CICLOS += 50
+                INTERVAL = 1
+                INTERVAL_AUX = INTERVAL
             linea = proceso.stdout.readline()
-            print(linea)
+            # print(linea)
             linea_decod = linea.strip()
             # Si ya hay celdas entrenamos el modelo
 
@@ -162,7 +172,9 @@ if __name__ == '__main__':
                 coordenadas = coger_datos_geo(LOCALIZACION_RUTA_ARCHIVO)
                 serving_cell = procesar_linea(linea_decod,coordenadas)
                 if len(celdas) != 0:
-                    t_modelo = train_model(params, celdas)
+                    t_modelo = train_model(params,
+                                           celdas
+                    )
                 # Deteccion de obstaculos grandes,
                 # es necesario al menos haber establecido un pequeño recorrido de referencia
                 if len(celdas) > 10 and t_modelo:
@@ -173,20 +185,20 @@ if __name__ == '__main__':
                     y_pred = t_modelo.predict(data)
                     print(f"RSRP previsto : {y_pred} || RSRP : {serving_cell['rsrp']}")
                     if abs(abs(y_pred) - abs(serving_cell['rsrp'])) <= DIFF_RSRP_MIN:
-                        nuevo_intervalo = interval * 1.25
-                        interval = min(10, nuevo_intervalo)
-                        interval_aux = interval
+                        nuevo_intervalo = INTERVAL * 1.25
+                        INTERVAL = min(INTERVALO_MAX, nuevo_intervalo)
+                        INTERVAL_AUX = INTERVAL
                     elif DIFF_RSRP_MIN < abs(abs(y_pred) - abs(serving_cell['rsrp'])) < DIFF_RSRP_MAX:
-                        nuevo_intervalo = interval
-                        interval = nuevo_intervalo
-                        interval_aux = interval
+                        nuevo_intervalo = INTERVAL
+                        INTERVAL = nuevo_intervalo
+                        INTERVAL_AUX = INTERVAL
                     elif abs(abs(y_pred) - abs(serving_cell['rsrp'])) > DIFF_RSRP_MAX:
-                        nuevo_intervalo = min(1,interval * 0.5)
-                        interval = max(0.25, nuevo_intervalo)
-                        interval_aux = interval
+                        nuevo_intervalo = min(1, INTERVAL * 0.5)
+                        INTERVAL = max(INTERVALO_MIN, nuevo_intervalo)
+                        INTERVAL_AUX = INTERVAL
                         if abs(abs(y_pred) - abs(serving_cell['rsrp'])) > DIFF_RSRP_EXTR or abs(abs(celdas[-1]['rsrp']) - abs(serving_cell['rsrp'])) > DIFF_RSRP_EXTR:
                             print("Obstáculo detectado")
-                print(f'Intervalo hasta el siguiente escaneo de {interval} segundos')
+                print(f'Intervalo hasta el siguiente escaneo de {INTERVAL} segundos')
 
                 celdas.append(serving_cell)
 
@@ -200,7 +212,7 @@ if __name__ == '__main__':
                 else:
                     celda_ref = serving_cell
                     print(json.dumps(serving_cell, indent=2))
-                time.sleep(interval)
+                time.sleep(INTERVAL)
             elif 'LTE PHY Cell Search Measure:'in linea_decod and 'SCell' in linea_decod:
                 latitud,longitud,altitud = coger_datos_geo(LOCALIZACION_RUTA_ARCHIVO)
                 match = re.search('PCI' + r'\s+(\S*)', linea_decod)
@@ -235,15 +247,15 @@ if __name__ == '__main__':
                             rssi = int(match.group(1))
                             if rssi < -130:
                                 print("Ausencia total de cobertura")
-                                time.sleep(interval)
+                                time.sleep(INTERVAL)
 
                     if len(celda_ref.keys()) > 0:
                         print(f"Volver a {celda_ref['latitude']},{celda_ref['longitude']}")
                     else:
                         print("Buscar localizacion de referencia")
                 else:
-                    inerval_aux = interval
-                    interval = 0
+                    inerval_aux = INTERVAL
+                    INTERVAL = 0
 
 
     #Fin de ejecucion
